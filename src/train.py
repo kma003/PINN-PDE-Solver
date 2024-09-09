376180bebb8d82503d1a66008e5730a1383149f0
kma003 <kmanderson003@gmail.com>
2024-09-09 15:57:08 -0700

    Outlined training process, setting up input data for LBFGS optimizer remains

--- src/train.py
+++ src/train.py
@@ -5,25 +5,43 @@ import torch.nn as nn
 from scipy.io import loadmat
 
 from defs import DATA_DIR
+from networks.losses import SupervisedLoss,PhysicsInformedLoss
 from networks.MLP_8 import MLP_8
 
-data_path = os.path.join(DATA_DIR,"cylinder_wake.mat")
-data = loadmat(data_path)
 
-net = MLP_8(num_inputs=3,num_outputs=2)
 
-def train():
-    pass
+def train(learning_rate=1e-4,num_epochs=100):
 
-class SupervisedLoss(nn.Module):
-    def __init__(self):
-        super(SupervisedLoss,self).__init__()
-        self.criterion = nn.MSELoss()
+    data_path = os.path.join(DATA_DIR,"cylinder_wake.mat")
+    data = loadmat(data_path)
 
-    def forward(self,u,v,u_pred,v_pred):
-        u_loss = self.criterion(u,u_pred)
-        v_loss = self.criterion(v,v_pred)
-        return u_loss + v_loss
+    lr = learning_rate
+    model = MLP_8(num_inputs=3,num_outputs=2)
+    optimizer = torch.optim.LBFGS(model.parameters())
+    criterion1 = SupervisedLoss()
+    criterion2 = PhysicsInformedLoss()
 
-class PhysicsInformedLoss(nn.Module):
-    pass
+    for epoch in range(num_epochs):
+        model.train()
+        x=y=t=u=v=p = None # TODO Set up input data
+
+        optimizer.zero_grad()
+        p_pred,psi = model(torch.Tensor([x,y,t]))
+
+        # Differentiate for predictions, calculate losses
+        u_pred = torch.autograd.grad(psi,y,grad_outputs=torch.ones_like(psi),create_graph=True)[0]
+        v_pred = -1*torch.autograd.grad(psi,u,grad_outputs=torch.ones_like(psi),create_graph=True)[0]
+        loss1 = SupervisedLoss(u,v,u_pred,v_pred)
+        loss2 = PhysicsInformedLoss(x,y,t,u_pred,v_pred)
+        loss = loss1 + loss2
+        loss.backward()
+        optimizer.step()
+
+        pass
+
+    return
+
+
+if __name__ == "__main__":
+
+    train()
